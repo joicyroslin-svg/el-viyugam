d6d8b106778f7a0a12907e872f2c230bdc20c215
joicy roslin <joicyroslin@gmail.com>
2026-05-18 15:14:38 +0530

    Enhanced validation and request handling

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,7 +6,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const port = process.env.PORT ?? 3001;
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+  new ValidationPipe({
+    whitelist: true,
+    transform: true,
+    forbidNonWhitelisted: true,
+  }),
+);
   await app.listen(port);
 
   console.log(`Application is running on: http://localhost:${port}`);
